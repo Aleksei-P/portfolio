af492119ec25d62891409f0ddc98112a85a92f90
Alex <sdspb@yahoo.com>
2023-02-16 20:57:41 -0800

    new window link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,19 +4,19 @@ export default function Portfolio() {
       <h2>Portfolio</h2>
       <div className="portfolio-container">
         <div>
-          <a href="https://my-simple-note.netlify.app/">
+          <a href="https://my-simple-note.netlify.app/" target="_blank">
             <img src="/img/notes.jpg" alt="notes" />
           </a>
           <p>Notes</p>
         </div>
         <div>
-          <a href="https://aleksei-p.github.io/weather/">
+          <a href="https://aleksei-p.github.io/weather/" target="_blank">
             <img src="/img/weather.jpg" alt="weather" />
           </a>
           <p>Weather</p>
         </div>
         <div>
-          <a href="https://aleksei-p.github.io/portland-portland/">
+          <a href="https://aleksei-p.github.io/portland-portland/" target="_blank">
             <img src="/img/portland.jpg" alt="portland" />
           </a>
           <p>From Portland to Portland</p>
